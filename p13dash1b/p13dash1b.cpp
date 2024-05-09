f852a54c567138b8f741f9c346a1ffc49c6403d9
Caleb Collert <c-collert.1@onu.edu>
2024-05-08 23:29:57 -0400

    P13B-5

--- p13dash1b/p13dash1b.cpp
+++ p13dash1b/p13dash1b.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 
 bool isLeapYear(int year);
 int daysInMonth(int month, int year);
 int dayOfWeek(int month, int day, int year);
 void generateCalendar(int month, int year, string calendar[7][7]);
+int monthNameToValue(string monthName);
+void outputCalendarToFile(string& filename, string& monthName, int year, string calendar[7][7]);
 int main() {
 	int year, month;
+	string monthName;
 	string calendar[7][7];
 
 	do {
-		cout << "Enter a month and year or Q to quit: ";
-		cin >> month >> year;
-		if (cin.fail()) {
+		cout << "Enter a month name and year or Q to quit: ";
+		cin >> monthName;
+		if (monthName == "Q" || monthName == "q") {
 			return 0;
 		}
+		cin >> year;
+		month = monthNameToValue(monthName);
 
 		generateCalendar(month, year, calendar);
+		string filename = monthName.substr(0,3) + to_string(year) + ".txt";
+		outputCalendarToFile(filename, monthName, year, calendar);
 
-		cout << month << year << endl;
+		cout << monthName<< " " << year << endl;
 		for (int i = 0; i < 6; ++i) {
 			for (int j = 0; j < 7; ++j) {
 				cout << setw(3) << calendar[i][j] << " ";
@@ -120,4 +128,51 @@ void generateCalendar(int month, int year, string calendar[7][7]) {
 			++row;
 		}
 	}
+}
+
+int monthNameToValue(string monthName) {
+	if (monthName == "January")
+		return 1;
+	else if (monthName == "February")
+		return 2;
+	else if (monthName == "March")
+		return 3;
+	else if (monthName == "April")
+		return 4;
+	else if (monthName == "May")
+		return 5;
+	else if (monthName == "June")
+		return 6;
+	else if (monthName == "July")
+		return 7;
+	else if (monthName == "August")
+		return 8;
+	else if (monthName == "September")
+		return 9;
+	else if (monthName == "October")
+		return 10;
+	else if (monthName == "November")
+		return 11;
+	else if (monthName == "December")
+		return 12;
+	else return 0;
+}
+
+void outputCalendarToFile(string& filename, string& monthName, int year, string calendar[7][7]) {
+	ofstream outFile(filename);
+
+	if (!outFile.is_open()) {
+		cout << "Error opening file " << filename << endl;
+		return;
+	}
+
+	outFile << monthName.substr(0, 3) + to_string(year) + ".txt";
+	for (int i = 0; i < 6; ++i) {
+		for (int j = 0; j < 7; ++j) {
+			outFile << setw(3) << calendar[i][j] << " ";
+		}
+		outFile << endl;
+	}
+
+	outFile.close();
 }
