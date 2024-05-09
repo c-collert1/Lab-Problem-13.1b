c7c9a5d0efa67ad312f8ca16b60f0c50467ad894
Caleb Collert <c-collert.1@onu.edu>
2024-05-08 21:45:15 -0400

    Completed P13-B1

--- p13dash1b/p13dash1b.cpp
+++ p13dash1b/p13dash1b.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+bool isLeapYear(int year);
 int main() {
-
+	int year;
+	do {
+		cout << "Enter a year or Q to quit: ";
+		cin >> year;
+		if (!cin.fail()) {
+			if (isLeapYear(year)) {
+				cout << year << " is a leap year" << endl;
+			}
+			else {
+				cout << year << " is not a leap year" << endl;
+			}
+		}
+	} while (!cin.fail());
+}
+bool isLeapYear(int year) {
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
