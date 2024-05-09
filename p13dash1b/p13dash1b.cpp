98959dfeea8b13d71ac29dc15e194201dd892fd1
Caleb Collert <c-collert.1@onu.edu>
2024-05-08 22:06:58 -0400

    P13B-2

--- p13dash1b/p13dash1b.cpp
+++ p13dash1b/p13dash1b.cpp
@@ -2,19 +2,50 @@
 using namespace std;
 
 bool isLeapYear(int year);
+int daysInMonth(int month, int year);
 int main() {
-	int year;
+	int year, month;
 	do {
-		cout << "Enter a year or Q to quit: ";
-		cin >> year;
-		if (!cin.fail()) {
-			if (isLeapYear(year)) {
-				cout << year << " is a leap year" << endl;
-			}
-			else {
-				cout << year << " is not a leap year" << endl;
-			}
+		cout << "Enter a month and year or Q to quit: ";
+		cin >> month >> year;
+		if (month == 1) {
+			cout << "January " << year << " has " << daysInMonth(month, year) << " days.";
 		}
+		if (month == 2) {
+			cout << "February " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		if (month == 3) {
+			cout << "March " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		if (month == 4) {
+			cout << "April " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		if (month == 5) {
+			cout << "May " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		if (month == 6) {
+			cout << "June " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		if (month == 7) {
+			cout << "July " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		if (month == 8) {
+			cout << "August " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		if (month == 9) {
+			cout << "September " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		if (month == 10) {
+			cout << "October " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		if (month == 11) {
+			cout << "November " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		if (month == 12) {
+			cout << "December " << year << " has " << daysInMonth(month, year) << " days.";
+		}
+		cout << endl;
+
 	} while (!cin.fail());
 }
 bool isLeapYear(int year) {
@@ -23,5 +54,47 @@ bool isLeapYear(int year) {
 	}
 	else {
 		return false;
+	}
+}
+int daysInMonth(int month, int year) {
+	
+	if (month == 1) {
+		return 31;
+	}
+	if (month == 2) {
+		if (isLeapYear(year)) {
+			return 29;
+		}
+		return 28;
+	}
+	if (month == 3) {
+		return 31;
+	}
+	if (month == 4) {
+		return 30;
+	}
+	if (month == 5) {
+		return 31;
+	}
+	if (month == 6) {
+		return 30;
+	}
+	if (month == 7) {
+		return 31;
+	}
+	if (month == 8) {
+		return 31;
+	}
+	if (month == 9) {
+		return 30;
+	}
+	if (month == 10) {
+		return 31;
+	}
+	if (month == 11) {
+		return 30;
+	}
+	if (month == 12) {
+		return 31;
 	}
 }
